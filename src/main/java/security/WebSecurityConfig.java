e1e57ad853d3d1d9c05d3fe6c06801a6b3339da3
vvisnia <ziomek131212@wp.pl>
2016-01-18 21:07:42 +0100

    Changes

--- src/main/java/security/WebSecurityConfig.java
+++ src/main/java/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.AuthenticationManagerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -22,6 +23,20 @@ public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 		.withUser("user")
 		.password("user")
 		.roles("User");
+}
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+
+    http.authorizeRequests()
+    .antMatchers("/admin/**").access("hasRole('Admin')")
+    .and()
+        .formLogin().loginPage("/login").failureUrl("/login?error")
+        .usernameParameter("username").passwordParameter("password")        
+    .and()
+        .logout().logoutSuccessUrl("/login?logout")
+    .and()
+        .csrf();         
 }
 
 }
