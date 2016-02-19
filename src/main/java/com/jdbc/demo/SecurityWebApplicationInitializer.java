bc7335396c790d64638407fbe67c62dfd34074cd
vvisnia <ziomek131212@wp.pl>
2016-02-19 07:43:42 +0100

    Simple auth working

---
+++ src/main/java/com/jdbc/demo/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.jdbc.demo;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer
+      extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfig.class);
+    }
+}
